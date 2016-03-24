40c5363f1b587c1b7fe46322ad3cbe66fcd271fb
millidavids <david.yurek@realpage.com>
2016-03-24 13:13:48 -0400

    command progress

--- app/Console/Commands/CleanTemplate.php
+++ app/Console/Commands/CleanTemplate.php
@@ -3,8 +3,10 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
 
-class CleanTemplate extends Command {
+class CleanTemplate extends Command
+{
 
     /**
      * The console command name.
@@ -27,7 +29,59 @@ class CleanTemplate extends Command {
      */
     public function fire()
     {
-        $this->info('it works!');
+        if ($this->option('force')) {
+            $db_reset = 'yes';
+            $migrations = 'yes';
+            $seeds = 'yes';
+        } else {
+            $db_reset = $this->confirm('Reset database migrations?', true);
+            $migrations = $this->confirm('Remove database migrations?',
+                true);
+            $seeds = $this->confirm('Remove database seeds?', true);
+        }
+        if ($db_reset) {
+            exec('docker-compose run --rm fpm php artisan migrate:reset');
+        }
+        if ($migrations) {
+            $filename = database_path('migrations/2016_03_16_122149_create_quotes_table.php');
+            $this->deleteFile($filename, 'Example migration');
+        }
+        if ($seeds) {
+            $filename = database_path('seeds/QuotesTableSeeder.php');
+            $this->deleteFile($filename, 'Example seed');
+
+            $fname = database_path('seeds/DatabaseSeeder.php');
+            $fhandle = fopen($fname, "r");
+            $content = fread($fhandle, filesize($fname));
+
+            $content = str_replace('$this->call(\'QuotesTableSeeder\');', "",
+                $content);
+
+            $fhandle = fopen($fname, "w");
+            fwrite($fhandle, $content);
+            fclose($fhandle);
+        }
+    }
+
+    private function deleteFile($filename, $type)
+    {
+        if (file_exists($filename)) {
+            unlink($filename);
+        } else {
+            $this->warn("$type already deleted.");
+        }
+    }
+
+    protected function getOptions()
+    {
+        return array(
+            array(
+                'force',
+                'f',
+                InputOption::VALUE_NONE,
+                'Force yes.'
+            )
+        );
     }
 
 }
